ba66775f1f878901b852a0827af53880f0b20ca4
Ed Morley <501702+edmorley@users.noreply.github.com>
2020-04-24 11:04:26 +0100

    Trigger a new Heroku-20 stack image

--- bin/publish-to-dockerhub.sh
+++ bin/publish-to-dockerhub.sh
@@ -29,23 +29,23 @@ if [ "$STACK" != "cedar-14" ]; then
   docker push $dateBuildTag
 fi
 
-if [ -n "$TRAVIS_TAG" ]; then
-  releaseTag="${IMAGE_TAG}.${TRAVIS_TAG}"
-  releaseBuildTag="${IMAGE_TAG}-build.${TRAVIS_TAG}"
+if true; then
+  # releaseTag="${IMAGE_TAG}.${TRAVIS_TAG}"
+  # releaseBuildTag="${IMAGE_TAG}-build.${TRAVIS_TAG}"
   latestTag="${IMAGE_TAG}"
   latestBuildTag="${IMAGE_TAG}-build"
 
-  docker tag $nightlyTag $releaseTag
+  # docker tag $nightlyTag $releaseTag
   docker tag $nightlyTag $latestTag
 
-  docker push $releaseTag
+  # docker push $releaseTag
   docker push $latestTag
 
   if [ "$STACK" != "cedar-14" ]; then
-    docker tag $nightlyBuildTag $releaseBuildTag
+    # docker tag $nightlyBuildTag $releaseBuildTag
     docker tag $nightlyBuildTag $latestBuildTag
 
-    docker push $releaseBuildTag
+    # docker push $releaseBuildTag
     docker push $latestBuildTag
   else
     docker tag $nightlyTag heroku/cedar:latest
